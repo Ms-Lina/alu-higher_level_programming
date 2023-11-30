4abc50a2017efd3db1f47514d268bde992727625
Ms-Lina <linamis8008@gmail.com>
2023-11-30 11:42:24 -0800

    net

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""send letter"""
+"""take a url and display the values"""
+
+
 import requests
 import sys
 
-if __name__ == '__main__':
-    try:
-        lett = sys.argv[1]
-    except IndexError:
-        lett = ""
-    response = requests.post(
-        "http://0.0.0.0:5000/search_user",
-        data={"q": lett}
-    )
-    try:
-        json_response = response.json()
-        if response.headers.get("Content-Type") == 'application/json':
-            if len(json_response) > 0:
-                print("[{}] {}".format(
-                    json_response["id"],
-                    json_response["name"])
-                )
-            else:
-                print("No result")
-    except:
-        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    """send the email"""
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code <= 400:
+        print("{}".format(response.text))
+    else:
+        print("Error code: {}".format(response.status_code))
